5b5856c4d11560e81edb9a7912e882847449bfe4
Raúl Cruz <rcruz@abaenglish.com>
2016-10-26 10:54:13 +0200

    fix(zgen): zgen reset before update

--- install/zgen.rb
+++ install/zgen.rb
@@ -8,7 +8,7 @@ module Zgen
   def update
     if ENV['SHELL'] == '/usr/local/bin/zsh' then
       Log.info 'Updating zgen'
-      Command.execute 'zsh -c "source ~/.zshrc && zgen update && source ~/.zshrc"'
+      Command.execute 'zsh -c "source ~/.zshrc && zgen reset && zgen update && source ~/.zshrc"'
       Log.success 'Updating zgen'
     end
   end
