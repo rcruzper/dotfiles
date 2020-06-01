79104213a4b0ceb715ff9062980a12e8df4bc793
Raúl Cruz <rcruzper@gmail.com>
2020-06-01 18:48:21 +0200

    add new aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,10 @@
-alias ls='ls -laG'
+alias ls='ls -G'
+alias la='ls -laG'
+
+alias watch='watch '
+
+alias i.='(idea $PWD &>/dev/null &)'
+alias c.='(code $PWD &>/dev/null &)'
+alias o.='open .'
+
+alias dotfiles="cd ~/.dotfiles"
