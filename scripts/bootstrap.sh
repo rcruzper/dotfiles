6b6ada91f2cfb8082390add0fec1c6763a759d65
Raúl Cruz <rcruzper@gmail.com>
2020-06-07 10:08:41 +0200

    improve DOTFILES_ROOT variable declaration

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,11 +5,15 @@ set -euo pipefail
 cd "$(dirname "$0")/.." > /dev/null
 DOTFILES_ROOT=$(pwd -P)
 
-. $DOTFILES_ROOT/scripts/tools/logging.sh --source-only
+export DOTFILES_ROOT
 
-$DOTFILES_ROOT/macos/macos.sh
-$DOTFILES_ROOT/git/git.sh
-$DOTFILES_ROOT/brew/brew.sh
-$DOTFILES_ROOT/zsh/zsh.sh
-$DOTFILES_ROOT/scripts/symlink.sh
-$DOTFILES_ROOT/vim/vim.sh
+# shellcheck source=-~/.dotfiles/scripts/tools/logging.sh
+source "$DOTFILES_ROOT/scripts/tools/logging.sh"
+
+"$DOTFILES_ROOT/macos/macos.sh"
+"$DOTFILES_ROOT/git/git.sh"
+"$DOTFILES_ROOT/brew/brew.sh"
+"$DOTFILES_ROOT/zsh/zsh.sh"
+"$DOTFILES_ROOT/scripts/symlink.sh"
+"$DOTFILES_ROOT/vim/vim.sh"
+
