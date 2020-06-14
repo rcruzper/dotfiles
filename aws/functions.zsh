1a4ac904fdf8f27a4c3f0fc96602bab72cf528a0
Raúl Cruz <rcruzper@gmail.com>
2020-06-14 11:14:54 +0200

    replace dash for underscore

--- aws/functions.zsh
+++ aws/functions.zsh
@@ -1,7 +1,7 @@
-function ip_from_instance() {
+function ip-from-instance() {
     aws ec2 describe-instances --filters "{\"Name\":\"tag:Name\", \"Values\":[\"$1\"]}" --query='Reservations[0].Instances[0].PrivateIpAddress' | tr -d '"'
 }
 
 function ssh-aws() {
-    ssh "${2:=ec2-user}"@"$(ip_from_instance "$1")"
+    ssh "${2:=ec2-user}"@"$(ip-from-instance "$1")"
 }
