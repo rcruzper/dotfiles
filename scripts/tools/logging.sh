d2a47bbc284fb4e545b48f98a6dc3d1e3bca7bef
Raúl Cruz <rcruzper@gmail.com>
2020-08-12 17:23:02 +0200

    remove colors for actions

--- scripts/tools/logging.sh
+++ scripts/tools/logging.sh
@@ -2,19 +2,19 @@
 
 set -euo pipefail
 
-green=$(tput setaf 2)
-yellow=$(tput setaf 3)
-blue=$(tput setaf 4)
-reset=$(tput sgr0)
+#green=$(tput setaf 2)
+#yellow=$(tput setaf 3)
+#blue=$(tput setaf 4)
+#reset=$(tput sgr0)
 
 function info() {
-    printf "[ ${blue}..${reset} ] %s\n" "$@"
+    printf "[ .. ] %s\n" "$@"
 }
 
 function success() {
-    printf "[ ${green}OK${reset} ] %s\n\n" "$@"
+    printf "[ OK ] %s\n\n" "$@"
 }
 
 function user() {
-    printf "[ ${yellow}??${reset} ] %s\n" "$@"
+    printf "[ ?? ] %s\n" "$@"
 }
