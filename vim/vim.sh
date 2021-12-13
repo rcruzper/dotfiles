05c616c02995d5732973f0f3024247ff4c8c4b6d
Raúl Cruz <rcruzper@gmail.com>
2021-12-13 16:08:33 +0100

    fix(vim): copy plug.vim as symlink

--- vim/vim.sh
+++ vim/vim.sh
@@ -7,9 +7,9 @@ source "$DOTFILES_ROOT/scripts/tools/logging.sh"
 info 'Installing plug.vim'
 mkdir -p ~/.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp "$DOTFILES_ROOT/vim/plug.vim" ~/.vim
+ln -snf "$DOTFILES_ROOT/vim/plug.vim" ~/.vim
 success 'Installing plug.vim'
 
 info 'Updating vim plugins'
-vim +PlugUpgrade +PlugUpdate +PlugInstall +qall
+vim +PlugClean! +PlugUpgrade +PlugUpdate +PlugInstall +qall
 success 'Updating vim plugins'
