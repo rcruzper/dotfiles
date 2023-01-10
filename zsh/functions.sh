06db39a33d8eef7cb0d0192da03177e572523431
Raúl Cruz <rcruzper@gmail.com>
2023-01-10 13:45:00 +0100

    zsh: group functions

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -13,3 +13,11 @@ function z() {
 
   __zoxide_z "$1"
 }
+
+function ip-from-instance() {
+    aws ec2 describe-instances --filters "{\"Name\":\"tag:Name\", \"Values\":[\"$1\"]}" --query='Reservations[0].Instances[0].PrivateIpAddress' | tr -d '"'
+}
+
+function ssh-aws() {
+    ssh "${2:=ec2-user}"@"$(ip-from-instance "$1")"
+}
