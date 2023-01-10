7ab9487842b7ecdb2b390c9d6cbff75e98e95bb1
Raúl Cruz <rcruzper@gmail.com>
2023-01-10 13:03:30 +0100

    zsh: remove slow sdkman load

--- sdkman/sdkman.sh
+++ sdkman/sdkman.sh
@@ -13,11 +13,17 @@ then
     success 'Installing sdkman'
 else
     info 'Flushing sdkman'
-    zsh -i -c "sdk flush"
-    zsh -i -c "sdk flush broadcast"
+    zsh -i -c 'export SDKMAN_DIR="$HOME/.sdkman" && \
+        [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && \
+        source "$HOME/.sdkman/bin/sdkman-init.sh" && \
+        sdk flush && \
+        sdk flush broadcast'
     success 'Flushing sdkman'
     info 'Updating sdkman'
-    zsh -i -c "sdk selfupdate"
-    zsh -i -c "sdk update"
+    zsh -i -c 'export SDKMAN_DIR="$HOME/.sdkman" && \
+        [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && \
+        source "$HOME/.sdkman/bin/sdkman-init.sh" && \
+        sdk selfupdate && \
+        sdk update'
     success 'Updating sdkman'
 fi
