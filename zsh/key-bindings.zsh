71616575a4f7c16a914dddc9fab970383626314f
Raúl Cruz <rcruzper@gmail.com>
2023-01-10 11:50:35 +0100

    zsh: remove control-r binding

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -8,7 +8,5 @@ bindkey '^[[1;9C' emacs-forward-word                # [Option-LeftArrow] - move 
 bindkey '^[[H' beginning-of-line                    # [Fn-RightArrow] - move to the beginning of the line
 bindkey '^[[F' end-of-line                          # [Fn-LeftArrow] - move to the end of the line
 bindkey '^[[3~' delete-char                         # [Fn-Delete] - delete the character under the cursor
-bindkey "^R" history-search-multi-word              # [Control-R] - search backward incrementally for a specified string
 bindkey '^[[Z' reverse-menu-complete                # [Shift-TAB] - backward TAB in completions
 bindkey '^H' backward-delete-word                   # [Control-H] - delete the last word
-
