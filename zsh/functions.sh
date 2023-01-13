cbceee5af9f2c40a3775a3398b522b5edd13d748
Raúl Cruz <rcruzper@gmail.com>
2023-01-13 10:41:38 +0100

    zsh: add function to get main branch name of a git repository

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -30,6 +30,20 @@ function ssh-aws() {
 }
 
 # git functions
+
+# Check if main exists and use instead of master
+function git_main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local ref
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default}; do
+    if command git show-ref -q --verify $ref; then
+      echo ${ref:t}
+      return
+    fi
+  done
+  echo master
+}
+
 # gcheckout - checkout local|remote branch
 function gcheckout() {
     if [[ -n $1 ]] then
