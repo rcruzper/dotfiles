86caa5c5ba6e6a6147fc3d92c13d971ed45d4750
Raúl Cruz <rcruzper@gmail.com>
2023-01-16 14:44:41 +0100

    sdkman: install last java version

--- sdkman/sdkman.sh
+++ sdkman/sdkman.sh
@@ -9,9 +9,20 @@ if [ ! -f ~/.sdkman/bin/sdkman-init.sh ]
 then
     info 'Installing sdkman'
     curl -s "https://get.sdkman.io" | bash
-    # TODO How to install java after sdkman installation
+    ln -sf "$DOTFILES_ROOT/sdkman/config" "$HOME/.sdkman/etc/config"
+    zsh -i -c 'export SDKMAN_DIR="$HOME/.sdkman" && \
+        [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && \
+        source "$HOME/.sdkman/bin/sdkman-init.sh" && \
+        sdk install java'
     success 'Installing sdkman'
 else
+    info 'Updating java from sdkman'
+    ln -sf "$DOTFILES_ROOT/sdkman/config" "$HOME/.sdkman/etc/config"
+    zsh -i -c 'export SDKMAN_DIR="$HOME/.sdkman" && \
+        [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && \
+        source "$HOME/.sdkman/bin/sdkman-init.sh" && \
+        sdk install java'
+    success 'Updating java from sdkman'
     info 'Flushing sdkman'
     zsh -i -c 'export SDKMAN_DIR="$HOME/.sdkman" && \
         [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && \
