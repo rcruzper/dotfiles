ccb622a7c10bb850cb99a376973f17bd99d37050
Raúl Cruz <rcruzper@gmail.com>
2024-09-23 11:27:22 +0200

    neovim: update plugins during dotfiles update

--- nvim/nvim.sh
+++ nvim/nvim.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+source "$DOTFILES_ROOT/scripts/tools/logging.sh"
+
 ln -snf "$DOTFILES_ROOT/nvim/nvim" "$HOME/.config/nvim"
 
+info 'Updating neovim plugins'
+nvim --headless "+Lazy! sync" +qa
+success 'Updating neovim plugins'
