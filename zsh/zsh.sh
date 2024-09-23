d77669be22d0b0ccc2af9615ea83f610ab0349c2
Raúl Cruz <rcruzper@gmail.com>
2024-09-23 12:28:32 +0200

    zsh: update paths on installation file

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -15,7 +15,7 @@ fi
 # Set zsh as default shell
 if [[ -n $SHELL ]]
 then
-    if [ "$SHELL" != "/opt/homebrew/bin/zsh" ]
+    if [ "$SHELL" != "/bin/zsh" ]
     then
         info 'Setting up zsh as default shell'
         if ! grep -q /opt/homebrew/bin/zsh /etc/shells; then
@@ -29,10 +29,10 @@ then
 fi
 
 # Install zinit
-if [ ! -f ~/.zinit/bin/zinit.zsh ]
+if [ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]
 then
     info 'Installing zinit'
-    zsh -c "$(sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)")"
+    zsh -c "$(sh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)")"
     success 'Installing zinit'
 else
     info 'Updating zinit'
