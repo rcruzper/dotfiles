acc5110793113a87e2675debfd4f41f606d648c8
Raúl Cruz <rcruzper@gmail.com>
2024-09-30 12:25:48 +0200

    zsh: update zinit installation paths

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -1,7 +1,6 @@
 export EDITOR='nvim'
 export KUBE_EDITOR='nvim'
 
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export HOMEBREW_NO_ANALYTICS=true
 
 export FZF_DEFAULT_OPTS="--height 60% --color bg+:240"
@@ -15,7 +14,6 @@ export HISTSIZE=30000
 export SAVEHIST=30000
 
 export FZF_DEFAULT_COMMAND='fd --type f'
-# export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_COMMAND="git --no-pager diff --name-only; git ls-files --others --exclude-standard"
 
 # Configure completions: https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
