894fc1b9080620ebf377c6b0dc2b6500664ee660
Raúl Cruz <rcruzper@gmail.com>
2026-07-10 14:49:37 +0200

    tmux: auto-install tpm and plugins

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -6,6 +6,18 @@ source "$DOTFILES_ROOT/scripts/tools/logging.sh"
 
 ln -snf "$DOTFILES_ROOT/tmux/tmux" "$HOME/.config/tmux"
 
-# nvim --headless "+Lazy! sync" +qa
-# success 'Updating neovim plugins'
+TPM_DIR="$HOME/.config/tmux/plugins/tpm"
 
+if [ ! -d "$TPM_DIR" ]; then
+    info 'Installing tmux plugin manager (tpm)'
+    git clone --depth 1 https://github.com/tmux-plugins/tpm "$TPM_DIR"
+    success 'Installing tmux plugin manager (tpm)'
+else
+    info 'Updating tmux plugin manager (tpm)'
+    git -C "$TPM_DIR" pull --ff-only
+    success 'Updating tmux plugin manager (tpm)'
+fi
+
+info 'Installing/updating tmux plugins'
+"$TPM_DIR/bin/install_plugins"
+success 'Installing/updating tmux plugins'
